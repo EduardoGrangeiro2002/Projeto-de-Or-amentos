1716f284b66ac016f8ffee31b9779387c00786a9
EduardoGrangeiro2002 <eduatdoaugusto503@gmail.com>
2022-01-08 13:16:21 -0300

    Entidade adicionada

--- src/modules/Budgets/Entities/Budgets.ts
+++ src/modules/Budgets/Entities/Budgets.ts
@@ -1,17 +1,37 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity('budgets')
 class Budgets {
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   cep: string;
 
+  @Column()
   number: string;
 
+  @Column()
   client: string;
 
+  @Column()
   cnpj: string;
 
+  @Column()
   parts: string[];
 
+  @Column()
   status: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+  }
 }
 
 export { Budgets };
